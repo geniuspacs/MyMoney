d61f1426167b70e3ae34e8702453f8100047f7ae
Eugenio <eugenio_8778@hotmail.com>
2018-02-16 15:01:28 +0100

    commit 1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import * as mongodb from "mongodb-stitch";
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  stitch: mongodb;
 
+  constructor(public navCtrl: NavController) {
+    console.log(this.stitch);
   }
 
 }
